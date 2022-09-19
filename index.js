9250977dae6c2168a98ba71aacf27c9521f3a593
Jenish Cyclewala <cyclewala@users.noreply.github.com>
2022-09-19 22:57:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function lakePinTapped(lake) {
 	dropPin()
 }
 
-const dropPinObj = {"coordinate":"43.7246148,-80.0301477","lakeName":"Brampton","pinColor":"#5617c2","description":"water is safe"}
+const dropPinObj = {"coordinate":"43.6534689,-80.0376125","lakeName":"Hello World","pinColor":"#5617c2","description":"water is safe"}
 
 function dropPin() {
 try {
